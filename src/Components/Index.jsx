9e3945ea171d3a93e03811d9a685bb711b2e8f37
Layla Rodas <rodas.layla@gmail.com>
2024-02-03 16:28:44 +0100

    estilos a la home page

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 export const Index = () => {
   return (
-    <div>
+    <div className='home'>
       <h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste aperiam aut, soluta quae minus explicabo cum modi quos quasi illum eligendi neque molestiae officia. Amet dignissimos eum voluptates non provident.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. <strong>Iste aperiam aut</strong>, soluta quae minus explicabo cum modi quos quasi illum eligendi neque molestiae officia. Amet dignissimos eum voluptates non provident.
       </h1>
 
-      <h2>Numquam sint dolorem aliquid similique expedita voluptate fugit tempore consectetur animi natus repudiandae <Link to="/contact">Contact me</Link></h2>
+      <h2>Numquam sint dolorem aliquid similique expedita voluptate fugi <Link to="/contact">Contact me</Link></h2>
 
       <section className='lastsProjects'>
-        <h2>Some of my projects</h2>
+        <h2 className='heading'>Some of my projects</h2>
         <p>These are some of my web development works</p>
 
         <div className='projects'>
